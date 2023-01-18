f98dd109a4609fe78ef4201987eeb7983c1c7290
WhatWR <kul.b@ku.th>
2023-01-19 01:48:39 +0700

    commit

--- src/scenes/MySkills.tsx
+++ src/scenes/MySkills.tsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion'
 
 const MySkills = () => {
   return (
-    <section id="Experience" className="pt-10 pb-24">
+    <section id="experience" className="pt-10 pb-24">
       {/* HEADER AND IMAGE SECTION */}
       <div className="md:flex md:justify-between md:gap-16 mt-32">
         <motion.div
